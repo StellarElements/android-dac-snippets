9dd728b711fbd87fc5d063e8e3f8bbc5c861f9c6
Ramon Cadeaux <ramon.cadeaux1@accesscfa.com>
2026-09-16 14:21:58 -0400

    updates after spotless apply

--- compose/snippets/src/main/java/com/example/compose/snippets/components/Card.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/Card.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -33,7 +32,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -119,9 +117,7 @@ fun OutlinedCardExample() {
         Text(
             text = "Outlined",
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,
+                .padding(16.dp),
         )
     }
 }
